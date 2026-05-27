1e63fb2fac86d4b04c7e7da47665ef3b1f380345
Joaquin Almada <joaalmada.7@gmail.com>
2026-05-27 16:10:27 -0300

    Agregamos configuracion dma

--- src/plataform/dma_lpc1769.c
+++ src/plataform/dma_lpc1769.c
@@ -4,12 +4,55 @@
  *  Created on: 27 may 2026
  *      Author: joaquin
  */
-
 #include "plataform.h"
 #include "lpc17xx_gpdma.h"
 
 static GPDMA_LLI_T dmaLli;
+#define cant_transf 60 //Vamos a tomar 60 muestras como maximo
+static volatile uint32_t muestrasADC[60];
 
 void configDma(void) {
-
+	GPDMA_Init();
+	GPDMA_Endpoint_T srcc= {
+			.width= GPDMA_WORD,
+			.burst= GPDMA_BSIZE_1,
+			.increment= DISABLE
+	};
+	GPDMA_Endpoint_T dstt= {
+			.width= GPDMA_WORD,
+			.burst= GPDMA_BSIZE_1,
+			.increment= ENABLE
+	};
+	GPDMA_Channel_CFG_T dmaCfg= {
+			.channelNum= GPDMA_CH_0,
+			.transferSize= cant_transf,
+			.type= GPDMA_P2M,
+			.srcMemAddr= 0,
+			.dstMemAddr= (uint32_t)&muestrasADC,
+			.srcConn= GPDMA_ADC,
+			.dstConn= 0,
+			.src= srcc,
+			.dst= dstt,
+			.intTC= ENABLE,
+			.intErr= DISABLE,
+			.linkedList= (uint32_t)&dmaLli
+	};
+	GPDMA_SetupChannel(&dmaCfg);
+	GPDMA_ChannelStart(GPDMA_CH_0);
 }
+
+void DMA_IRQHandler(void) {
+	GPDMA_ClearIntPending(GPDMA_CLR_INTTC, GPDMA_CH_0);
+	//TODO: Completar funcion
+	//TODO: Calcular promedio
+	//TODO: Decidir en donde enviamos los datos al DAC
+}
+
+//Devuelve la direccion del buffer
+volatile uint32_t *getAdcBuffer(void) {
+	return muestrasADC;
+}
+
+
+
+
--- src/plataform/plataform.h
+++ src/plataform/plataform.h
@@ -5,12 +5,16 @@
  *      Author: joaquin
  */
 
+#include "LPC17xx.h"
+#include <cr_section_macros.h>
+
 #ifndef PLATAFORM_PLATAFORM_H_
 #define PLATAFORM_PLATAFORM_H_
 
 void configAdc(void);
 void configDac(void);
 void configDma(void);
+volatile uint32_t *getAdcBuffer(void);
 void configTimer(void);
 void configUart(void);
 
