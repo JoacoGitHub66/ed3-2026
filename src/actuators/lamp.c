d863a3dd350b5eb3ed0de30c4feb76b3d270612c
Mati <matias.sacchi@mi.unc.edu.ar>
2026-05-29 21:53:49 -0300

    configuracion de actuadores

--- src/actuators/lamp.c
+++ src/actuators/lamp.c
@@ -6,3 +6,40 @@
  */
 
 
+#include "lamp.h"
+#include "LPC17xx.h"
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+
+void stopLamp(void){
+
+	GPIO_SetPinState(PORT_0, PIN_1, RESET);
+
+	return;
+}
+
+void startLamp(void){
+
+	GPIO_SetPinState(PORT_0, PIN_1, SET);
+
+	return ;
+}
+
+void configPinLamp(void){
+
+	//Todo: configurar pin para la lamparita
+
+	//pin 0.01 -> Lamp
+	PINSEL_CFG_T pinLamp;
+	pinLamp.pin = PIN_1;
+	pinLamp.port = PORT_0;
+	pinLamp.func = PINSEL_FUNC_00;
+	pinLamp.mode = PINSEL_TRISTATE;
+	pinLamp.openDrain = DISABLE;
+
+	PINSEL_ConfigPin(&pinLamp);
+
+	GPIO_SetDir(PORT_0 ,PIN_0, GPIO_OUTPUT);
+
+	return;
+}
--- src/actuators/lamp.h
+++ src/actuators/lamp.h
@@ -8,6 +8,10 @@
 #ifndef ACTUATORS_LAMP_H_
 #define ACTUATORS_LAMP_H_
 
+void startLamp();
 
+void stopLamp();
+
+void configPinLamp();
 
 #endif /* ACTUATORS_LAMP_H_ */
